b4061d8e88931e8170318ee277fe643b4bf4cc01
Davi Machado <brisolo@proton.me>
2025-04-24 17:59:49 -0300

    Add atividade 7 da lista 2

---
+++ Lista2/Atividade7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Atividade7;
+
+class Program
+{
+    static void Main()
+    {
+        const int n = 500;
+
+        double totBru = 0.0;
+        double totImp = 0.0;
+        double totLiq = 0.0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Nota {i}");
+
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Valor do produto: ");
+            double val = double.Parse(Console.ReadLine());
+
+            Console.Write("Imposto sobre o produto: ");
+            double imp = double.Parse(Console.ReadLine());
+
+            totBru += val;
+            totImp += imp;
+            totLiq += val - imp;
+
+            Console.WriteLine(); // quebra de linha entre notas
+        }
+
+        Console.WriteLine("---------- RESUMO ----------");
+        Console.WriteLine("Total Bruto: R$ " + totBru.ToString("F2"));
+        Console.WriteLine("Total Imposto: R$ " + totImp.ToString("F2"));
+        Console.WriteLine("Total Líquido: R$ " + totLiq.ToString("F2"));
+    }
+}
